518eb580afb6e0022a10048d5876ded7d4746aaa
PabloAMC <pmorenocf@alumnos.unex.es>
2016-07-04 13:43:13 +0200

    Update jump.py

--- plugins/jump.py
+++ plugins/jump.py
@@ -2,9 +2,17 @@ import plugin_interface as plugintypes
 import numpy as np
 from sklearn import svm
 import uinput
+import timeit
+import datetime
 
 
 class PluginCSVCollect(plugintypes.IPluginExtended):
+		def __init__(self, file_name="collect.csv", delim = ",", verbose=False):
+			now = datetime.datetime.now()
+			self.time_stamp = '%d-%d-%d_%d-%d-%d'%(now.year,now.month,now.day,now.hour,now.minute,now.second)
+			self.file_name = self.time_stamp
+			self.start_time = timeit.default_timer()
+
 '''
 	def __init__(self, verbose=False):
     source: http://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html#example-svm-plot-rbf-parameters-py
@@ -12,6 +20,7 @@ class PluginCSVCollect(plugintypes.IPluginExtended):
     source: https://github.com/tuomasjjrasanen/python-uinput/blob/master/README.rst
 
 '''
+
 
 	def activate(self):
 	  
@@ -29,10 +38,10 @@ class PluginCSVCollect(plugintypes.IPluginExtended):
 		print "Think of jumping in order to jump"
 
 	def __call__(self, sample):
-		clf.decision_function_shape = "ovr"
-		dec = clf.decision_function(sample)
-		
+'''		clf.decision_function_shape = "ovr"
+		dec = clf.decision_function(sample)'''
+
 '''here there is probably a mistake'''
-		if dec == True:
+		if (now.second % 20)==0:
 			with uinput.Device([uinput.KEY_SPACE]) as device:
 	    		device.emit_click(uinput.KEY_SPACE)
