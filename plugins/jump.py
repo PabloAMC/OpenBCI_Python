865e26820ef840029da703eda5914a509106ca4c
PabloAMC <pmorenocf@alumnos.unex.es>
2016-07-07 12:49:11 +0200

    modification of pluging jump in order to make it activate randomly once per second

--- plugins/jump.py
+++ plugins/jump.py
@@ -1,48 +1,21 @@
 import plugin_interface as plugintypes
-import numpy as np
-from sklearn import svm
 import uinput
-import timeit
-import datetime
+import random
 
 
-class PluginCSVCollect(plugintypes.IPluginExtended):
-	def __init__(self):
-		rbf_svc = svm.SVC(kernel='rbf')
-		clf = svm.SVC(C=0.5,gamma=1)
-		clf.fit(arrayX, arrayY)
-	
-	
-	
-	'''
-	def __init__(self, verbose=False)
-	source: http://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html#example-svm-plot-rbf-parameters-py
-	source: https://github.com/OpenBCI/OpenBCI_Python/blob/master/plugins/csv_collect.py
-	source: https://github.com/tuomasjjrasanen/python-uinput/blob/master/README.rst
-	'''
+class PluginJump(plugintypes.IPluginExtended):
+    def __init__(self):
+        self.dev = uinput.Device([uinput.KEY_SPACE])
 
+    def activate(self):
+        print "Activated"
 
-	def activate(self):
-	  
-		print "Activated"
+    def deactivate(self):
+        print "Deactivated"
 
-		events = (uinput.KEY_SPACE)
-		device = uinput.Device(events)
-		time.sleep(1)
-		# arrayX and arrayY are to be recorded
+    def show_help(self):
+        print "Think of jumping in order to jump"
 
-	def deactivate(self):
-		print "Deactivated"
-
-	def show_help(self):
-		print "Think of jumping in order to jump"
-
-	def __call__(self, sample):
-		clf.decision_function_shape = "ovr"
-		dec = clf.decision_function(sample)
-
-		'''here there is probably a mistake'''
-		if dec == True:
-			device = uinput.Device(events)
-
-	    		
+    def __call__(self, sample):
+        if random.random() > (1 - 1 /  250.0):
+            self.dev.emit_click(uinput.KEY_SPACE)
